443be184cb94b144355c668b2d729d151c4be21b
siobhan <6171295>
2017-11-22 14:51:27 +0800

    person 实体

---
+++ src/Person.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Administrator on 2017/11/22.
+ */
+
+/**
+ * <p>
+ * Title:
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Copyright: Copyright (c) 2016
+ * All rights reserved. 2017-11-22 14:50
+ * </p>
+ *
+ * @author siobhan.Zheng
+ * @version 1.0
+ */
+public class Person
+{
+
+}
